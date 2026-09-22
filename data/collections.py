d5e6ac94a8abfb752d09a08dd8d2f806ce39d82e
SillyJoy <326221024+SillyJoy@users.noreply.github.com>
2026-09-22 15:23:07 +0300

    Data update

--- data/collections.py
+++ data/collections.py
@@ -1,20 +1,46 @@
-insulators_db = [
+thermal_insulators_db = [
     {
         "id": 1,
         "title": "Rockwool Optima",
         "price": 2500,
+        "coefficient": 38,
         "description": "Каменная вата, хорошая цена, подходит для зимы.",
         "image_url": "http://localhost:9000/media/1.jpg",
         "video_url": "http://localhost:9000/media/1.mp4",
-        "status": 1
+        "status": 1,  # 1 - опубликован, 2 - черновик, 0 - удален
+        "likes": [100, 101, 103]
     },
     {
         "id": 2,
         "title": "DiROCK Аляска",
         "price": 790,
+        "coefficient": 36,
         "description": "Утеплитель, идеально подходящий для зимних домов.",
         "image_url": "http://localhost:9000/media/2.jpg",
         "video_url": "http://localhost:9000/media/2.mp4",
-        "status": 1
+        "status": 1,
+        "likes": [102, 106]
     },
+    {
+        "id": 3,
+        "title": "Технониколь Роклайт",
+        "price": 4000,
+        "coefficient": 32,
+        "description": "Средний утеплитель.",
+        "image_url": "http://localhost:9000/media/3.jpg",
+        "video_url": "http://localhost:9000/media/3.mp4",
+        "status": 2,
+        "likes": [109]
+    },
+    {
+        "id": 4,
+        "title": "ТеплоKnauf Коттедж",
+        "price": 2239,
+        "coefficient": 36,
+        "description": "Нормальный утеплитель.",
+        "image_url": "http://localhost:9000/media/4.jpg",
+        "video_url": "http://localhost:9000/media/4.mp4",
+        "status": 2,
+        "likes": [109, 127]
+    }
 ]
