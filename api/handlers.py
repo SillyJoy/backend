842f80ac0feda869951ee947c618c2099f605574
SillyJoy <326221024+SillyJoy@users.noreply.github.com>
2026-09-22 15:41:06 +0300

    Handler update

--- api/handlers.py
+++ api/handlers.py
@@ -4,24 +4,36 @@ from data.collections import thermal_insulators_db
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
-id = 0
 
 
 @router.get("/")
-def get_catalog(request: Request):
+def get_catalog(request: Request, id: int = 0, next: str = "false"):
+    nextid = id + 1
+    if next == "true":
+        id += 1
+        nextid = id + 1
+    if id < 0 or id > len(thermal_insulators_db):
+        id = 0
+        nextid = 1
+    if nextid > len(thermal_insulators_db):
+        nextid = 0
     return templates.TemplateResponse(
         request=request,
         name="feed.html",
-        context={"insulator": thermal_insulators_db[id], "liked": len(thermal_insulators_db[id]["likes"])}
+        context={"insulator": thermal_insulators_db[id], "liked": len(thermal_insulators_db[id]["likes"]), "nextid": nextid}
     )
 
 
 @router.get("/add")
 def get_catalog(request: Request):
+    for i in thermal_insulators_db:
+        if i["status"] == 2:
+            id = i["id"]
+            break
     return templates.TemplateResponse(
         request=request,
         name="add.html",
-        context={"insulator": thermal_insulators_db[id], "liked": len(thermal_insulators_db[id]["likes"])}
+        context={"insulator": thermal_insulators_db[id]}
     )
 
 
@@ -30,5 +42,5 @@ def get_catalog(request: Request):
     return templates.TemplateResponse(
         request=request,
         name="list.html",
-        context={"db": thermal_insulators_db, "liked": len(thermal_insulators_db[id]["likes"])}
+        context={"db": thermal_insulators_db}
     )
